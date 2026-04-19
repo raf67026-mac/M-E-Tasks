dacbfb07df6cc51794b290a310cfedce6388b4bb
rafif <rafif@MacBook-Air-rafif.local>
2026-04-19 10:40:04 +0300

    fix gmail smpt

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ const prisma = new PrismaClient();
 // ✅ EMAIL SETUP (ثابت وصحيح)
 const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
